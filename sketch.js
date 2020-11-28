ba0fa48f4acb7a6fcee5579c4fb99392460e9a01
StarfishSunset <72940354+StarfishSunset@users.noreply.github.com>
2020-11-29 01:24:58 +0800

    end...

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let data;
 let img;
+let h = (1, 46, 64);
 
 function preload() {
   data = loadJSON("meteorite.json");
@@ -24,28 +25,35 @@ for (let i = 0; i < data.meteorite.length; i++) {
   let annoyed = data.meteorite[i];
 
 noStroke();
-
-let m = map(annoyed.mass, )
-fill(m);  
+fill(255);  
 for (let x = 150; x < 500; x += 150) {
   for (let y = 150; y < 350; y += 90) {
 
     let d = annoyed.year
 ellipse(x, y, 20);
- let t = annoyed.recclass 
+let t = annoyed.recclass 
 let a = annoyed.x
 let b = annoyed.y
 textSize(20);
 text(t, a, b + 30)
 text(d, a, b + 50)
 
-  if (x > a && x < a && y > b && y < b) {
-let k = annoyed.name
-text(k, a + 30, b)
+push()
+let k = data.meteorite[i].mass;
+let o = data.meteorite[i].name;
+fill (h);
+text(k, a - 50, b +15)
+text(o, a - 50, b - 5)
+pop()
+    }
   }
 }
+}
+
+function mouseClicked() {
+  if (h === (1, 46, 64)) {
+    h = 255;
+  } else {
+    h = (1, 46, 64)
   }
 }
-
-}
-
